162350f259ba8f22b42f1ec9dc2799e89e11eec9
LK777-D <luka.dolbaia17@gmail.com>
2023-10-20 14:42:37 +0400

    font

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ViteFonts from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteFonts({
-      google: {
-        families: ["Paytone One", "Prompt", "Pacifico"],
-      },
-      custom: {
-        Robotech: "/fonts/ROBOTECH GP.ttf",
-      },
-    }),
-  ],
+  plugins: [react()],
 });
